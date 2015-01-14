5e53f4503d294c423a20d222460b7e5f8bb3f038
Sebastian M. Pedersen <sem.pedersen@stud.uis.no>
2015-01-14 19:26:40 +0100

    Removed Slick2D from pom.xml, we'll be importing it manually for now Added some boilerplate code

--- pong/src/main/java/Main.java
+++ pong/src/main/java/Main.java
@@ -1,5 +1,13 @@
+import org.newdawn.slick.AppGameContainer;
+import org.newdawn.slick.SlickException;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        try {
+            AppGameContainer app = new AppGameContainer(new Pong("Pong"));
+            app.start();
+        } catch (SlickException e) {
+            e.printStackTrace();
+        }
     }
 }
---
+++ pong/src/main/java/Pong.java
@@ -0,0 +1,25 @@
+import org.newdawn.slick.BasicGame;
+import org.newdawn.slick.GameContainer;
+import org.newdawn.slick.Graphics;
+import org.newdawn.slick.SlickException;
+
+public class Pong extends BasicGame {
+    public Pong(String title) {
+        super(title);
+    }
+
+    @Override
+    public void init(GameContainer container) throws SlickException {
+
+    }
+
+    @Override
+    public void update(GameContainer container, int delta) throws SlickException {
+
+    }
+
+    @Override
+    public void render(GameContainer container, Graphics g) throws SlickException {
+
+    }
+}
