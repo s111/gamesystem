e392d9f1712acdb8288a911c2618ad2a48f39a67
Sebastian M. Pedersen <sem.pedersen@stud.uis.no>
2015-01-14 22:38:09 +0100

    Added two-way communication between game and controller.

--- pong/src/main/java/com/github/s111/bachelor/pong/Pong.java
+++ pong/src/main/java/com/github/s111/bachelor/pong/Pong.java
@@ -8,9 +8,17 @@ import org.newdawn.slick.Graphics;
 import org.newdawn.slick.SlickException;
 
 import javax.websocket.DeploymentException;
+import javax.websocket.Session;
+import java.io.IOException;
 
 public class Pong extends BasicGame {
     private static Pong game = new Pong("Pong");
+
+    private static Session player1;
+    private static Session player2;
+
+    private static int players;
+
     private String message;
 
     public Pong(String title) {
@@ -19,6 +27,36 @@ public class Pong extends BasicGame {
 
     public static Pong getGame() {
         return game;
+    }
+
+    public static void addPlayer(Session session) {
+        int player;
+
+        if (players == 0 || (player1 != null && !player1.isOpen())) {
+            player1 = session;
+
+            player = 1;
+        } else {
+            player2 = session;
+
+            player = 2;
+        }
+
+        players++;
+
+        try {
+            session.getBasicRemote().sendText("Hello, I'm player " + player);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void removePlayer() {
+        players--;
+    }
+
+    public static int getPlayers() {
+        return players;
     }
 
     public void showMessage(String message) {
--- pong/src/main/java/com/github/s111/bachelor/pong/server/WebsocketServer.java
+++ pong/src/main/java/com/github/s111/bachelor/pong/server/WebsocketServer.java
@@ -2,13 +2,40 @@ package com.github.s111.bachelor.pong.server;
 
 import com.github.s111.bachelor.pong.Pong;
 
-import javax.websocket.OnMessage;
+import javax.websocket.*;
 import javax.websocket.server.ServerEndpoint;
+import java.io.IOException;
 
 @ServerEndpoint(value = "/ws")
 public class WebsocketServer {
+    private Session session;
+
+    @OnOpen
+    public void onOpen(Session session) {
+        this.session = session;
+
+        int players = Pong.getPlayers();
+
+        switch (players) {
+            case 0: Pong.addPlayer(session); break;
+            case 1: Pong.addPlayer(session); break;
+            default:
+                try {
+                    session.getBasicRemote().sendText("Already got 2 players");
+                    session.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+        }
+    }
+
+    @OnClose
+    public void onClose() {
+        Pong.removePlayer();
+    }
+
     @OnMessage
-    public void onMessage(String message) {
+    public void onMessage(Session session, String message) {
         Pong.getGame().showMessage(message);
     }
 }
