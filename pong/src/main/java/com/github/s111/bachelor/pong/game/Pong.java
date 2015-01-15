7e5d9a5c3b815e3ff8e2b05e3d9bf19f560bc2e3
Sebastian M. Pedersen <sem.pedersen@stud.uis.no>
2015-01-15 02:00:48 +0100

    Added the basic graphical Pong elements

--- pong/src/main/java/com/github/s111/bachelor/pong/game/Pong.java
+++ pong/src/main/java/com/github/s111/bachelor/pong/game/Pong.java
@@ -2,27 +2,63 @@ package com.github.s111.bachelor.pong.game;
 
 import com.github.s111.bachelor.pong.Application;
 import com.github.s111.bachelor.pong.network.GameSession;
-import org.newdawn.slick.BasicGame;
-import org.newdawn.slick.GameContainer;
-import org.newdawn.slick.Graphics;
-import org.newdawn.slick.SlickException;
+import org.newdawn.slick.*;
+import org.newdawn.slick.geom.Circle;
+import org.newdawn.slick.geom.Rectangle;
+import org.newdawn.slick.geom.Shape;
+import org.newdawn.slick.geom.Vector2f;
 
 public class Pong extends BasicGame {
+    private static final int MARGIN = 32;
+    private static final int PADDLE_WIDTH = 32;
+    private static final int PADDLE_HEIGHT = 128;
+    private static final int BALL_RADIUS = 8;
+
+    private int width;
+    private int height;
+
+    private int left;
+    private int right;
+
+    private int verticalCenter;
+    private int horizontalCenter;
+
     private GameSession gameSession = Application.getGameSession();
 
-    private String message;
+    private Rectangle player1;
+    private Rectangle player2;
+
+    private Circle ball;
 
     public Pong(String title) {
         super(title);
     }
 
-    public void showMessage(String message) {
-        this.message = message;
-    }
-
     @Override
     public void init(GameContainer container) throws SlickException {
+        setBoundaries(container);
+        instantiatePlayers();
 
+        ball = new Circle(horizontalCenter, verticalCenter, BALL_RADIUS);
+    }
+
+    private void setBoundaries(GameContainer container) {
+        width = container.getWidth();
+        height = container.getHeight();
+
+        left = MARGIN;
+        right = width - MARGIN - PADDLE_WIDTH;
+
+        verticalCenter = height / 2;
+        horizontalCenter = width / 2;
+    }
+
+    private void instantiatePlayers() {
+        player1 = new Rectangle(left, 0, PADDLE_WIDTH, PADDLE_HEIGHT);
+        player1.setCenterY(verticalCenter);
+
+        player2 = new Rectangle(right, 0, PADDLE_WIDTH, PADDLE_HEIGHT);
+        player2.setCenterY(verticalCenter);
     }
 
     @Override
@@ -32,8 +68,11 @@ public class Pong extends BasicGame {
 
     @Override
     public void render(GameContainer container, Graphics g) throws SlickException {
-        if (message != null) {
-            g.drawString(message, 32, 32);
-        }
+        g.fill(player1);
+        g.fill(player2);
+
+        g.fill(ball);
+
+        g.drawLine(horizontalCenter, 0, horizontalCenter, height);
     }
 }
--- pong/src/main/java/com/github/s111/bachelor/pong/network/GameSession.java
+++ pong/src/main/java/com/github/s111/bachelor/pong/network/GameSession.java
@@ -57,7 +57,5 @@ public class GameSession {
         } else {
             remote.sendText("Hello, Player2!");
         }
-
-        game.showMessage(message);
     }
 }
