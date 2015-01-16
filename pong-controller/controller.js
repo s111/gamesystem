278a2b9395c2890b51fe32e150cb90a6913b0f6c
Sebastian M. Pedersen <sem.pedersen@stud.uis.no>
2015-01-16 17:25:05 +0100

    Created a very simple controller for pong. I've used Phaser framework. The reason behind this is to simplifiy going fullscreen across different devices.

---
+++ pong-controller/controller.js
@@ -0,0 +1,53 @@
+var conn;
+
+var game = new Phaser.Game(800, 600, Phaser.AUTO, 'game', {preload: preload, create: create, update: update});
+var sprite;
+var target;
+
+var y = 0;
+
+var movePaddle = function(pos) {};
+
+function preload() {
+  conn = new WebSocket('ws://192.168.1.221:1234/ws');
+
+  conn.onopen = function (e) {
+    movePaddle = function(pos) {
+      conn.send(pos);
+    }
+  }
+
+  game.stage.backgroundColor = '#0000FF';
+}
+
+function create() {
+  game.scale.fullScreenScaleMode = Phaser.ScaleManager.EXACT_FIT;
+  game.scale.scaleMode = Phaser.ScaleManager.EXACT_FIT;
+  game.scale.refresh();
+
+  var graphics = game.add.graphics(0, 0);
+
+  graphics.beginFill(0xFF0000, 0.5);
+  graphics.drawRect(0, 0, game.stage.width - 32*2, 128);
+
+  sprite = game.add.sprite(32, 32);
+  sprite.addChild(graphics);
+  sprite.inputEnabled = true;
+  sprite.input.enableDrag();
+  sprite.input.allowHorizontalDrag = false;
+  sprite.input.boundsRect = new Phaser.Rectangle(32, 32, game.stage.width, game.stage.height - 32 - 128);
+
+  game.input.onDown.add(function(pointer) {
+    if (!game.scale.isFullScreen) {
+      game.scale.startFullScreen(false);
+    }
+
+    target = pointer.targetObject;
+  }, this);
+}
+
+function update() {
+  if (target && target.sprite === sprite && target.isDragged) {
+    movePaddle((sprite.y - 32)/(game.stage.height - 32*2 - 128));
+  }
+}
