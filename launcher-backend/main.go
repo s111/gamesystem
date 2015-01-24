1e3e867692505c65ef4dd772f4b2ea176013b48c
Sebastian M. Pedersen <sem.pedersen@stud.uis.no>
2015-01-24 16:44:57 +0100

    The launcher backend should now parse game.json files found in the games directory. For now game.json contains the name of the game and an array exec of how to execute it.
    TG-4 #closed

--- launcher-backend/main.go
+++ launcher-backend/main.go
@@ -1,7 +1,77 @@
 package main
 
-import "fmt"
+import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"os"
+	"os/exec"
+	"path/filepath"
+)
+
+type Game struct {
+	Name string
+	Exec []string
+}
+
+const path = "games"
+
+var games []Game
 
 func main() {
-	fmt.Println("Hello, World")
+	parseGames()
+	listGames()
+
+	for {
+		selectGame()
+	}
+}
+
+func parseGames() {
+	names, _ := ioutil.ReadDir(path)
+
+	for _, name := range names {
+		filename := filepath.Join(path, name.Name(), "game.json")
+
+		if _, err := os.Stat(filename); os.IsNotExist(err) {
+			fmt.Println("Warning: Found directory without game.json:", filename)
+
+			continue
+		}
+
+		file, _ := ioutil.ReadFile(filename)
+
+		var game Game
+
+		json.Unmarshal(file, &game)
+
+		games = append(games, game)
+	}
+}
+
+func listGames() {
+	fmt.Println("Availabe games:")
+
+	for i, game := range games {
+		fmt.Printf("\t\t%v. %v\n", i+1, game.Name)
+	}
+}
+
+func selectGame() {
+	var i int
+
+	_, err := fmt.Scanf("%d", &i)
+
+	i--
+
+	if err != nil || i < 0 || i >= len(games) {
+		return
+	}
+
+	startGame(games[i])
+}
+
+func startGame(game Game) {
+	exec.Command(game.Exec[0], game.Exec[1:]...).Run()
+
 }
