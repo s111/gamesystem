24a7ce2fe63947f1aeba09303d1c86c47469c986
Fredrik Kallevik <fredrik.kallevik@gmail.com>
2015-01-28 17:31:54 +0100

    Refactored controller code

--- triggerhappy-controller/controller.js
+++ triggerhappy-controller/controller.js
@@ -1,13 +1,8 @@
 var conn;
 
-var game = new Phaser.Game(800, 600, Phaser.AUTO, 'game', {preload: preload, create: create, update: update});
+var game = new Phaser.Game(800, 600, Phaser.AUTO, 'game', {preload: preload, create: create});
 
-var topLeft;
-var topMiddle;
-var topRight;
-var botLeft;
-var botMiddle;
-var botRight;
+var pos;
 
 var bttnHeight;
 var bttnWidth;
@@ -35,91 +30,26 @@ function create() {
   bttnWidth = game.stage.width / 3;
   bttnHeight = game.stage.height / 2;
 
-  // Draw graphics for buttons
+  var colors = [0xD24D57, 0xDB0A5B, 0xF64747, 0xF1A9A0, 0xD2527F, 0xF62459];
 
-  var topLeftGfx = game.add.graphics(0, 0);
-  topLeftGfx.beginFill(0xD24D57, 1);
-  topLeftGfx.drawRect(0, 0, bttnWidth, bttnHeight); // Top Left
- 
-  var topMiddleGfx = game.add.graphics(bttnWidth, 0); 
-  topMiddleGfx.beginFill(0xDB0A5B, 1);
-  topMiddleGfx.drawRect(0, 0, bttnWidth, bttnHeight); // Top Middle
+  for (i=0; i < 6; i++) {
+    g = game.add.graphics(0, 0);
+    g.beginFill(colors[i], 1);
+    g.drawRect((i%3) * bttnWidth, (i > 2 ? 1 : 0) * bttnHeight, bttnWidth, bttnHeight);
 
-  var topRightGfx = game.add.graphics(bttnWidth * 2, 0);
-  topRightGfx.beginFill(0xF64747, 1);
-  topRightGfx.drawRect(0, 0, bttnWidth, bttnHeight); // Top Right
-  
-  var botLeftGfx = game.add.graphics(0, bttnHeight);
-  botLeftGfx.beginFill(0xF1A9A0, 1);
-  botLeftGfx.drawRect(0, 0, bttnWidth, bttnHeight); // Bottom Left
-
-  var botMiddleGfx = game.add.graphics(bttnWidth, bttnHeight);
-  botMiddleGfx.beginFill(0xD2527F, 1);
-  botMiddleGfx.drawRect(0, 0, bttnWidth, bttnHeight); // Bottom Middle
-
-  var botRightGfx = game.add.graphics(bttnWidth * 2, bttnHeight);
-  botRightGfx.beginFill(0xF62459, 1);
-  botRightGfx.drawRect(0, 0, bttnWidth, bttnHeight); // Bottom Right
-
-  // Add sprites for buttons and listen for clicks
-
-  topLeft = game.add.sprite(0, 0);
-  topLeft.addChild(topLeftGfx);
-  topLeft.inputEnabled = true;
-  topLeft.events.onInputDown.add(onDown, this);
-
-  topMiddle = game.add.sprite(0, 0);
-  topMiddle.addChild(topMiddleGfx);
-  topMiddle.inputEnabled = true;
-  topMiddle.events.onInputDown.add(onDown, this);
-
-  topRight = game.add.sprite(0, 0);
-  topRight.addChild(topRightGfx);
-  topRight.inputEnabled = true;
-  topRight.events.onInputDown.add(onDown, this);
-
-  botLeft = game.add.sprite(0, 0);
-  botLeft.addChild(botLeftGfx);
-  botLeft.inputEnabled = true;
-  botLeft.events.onInputDown.add(onDown, this);  
-
-  botMiddle = game.add.sprite(0, 0);
-  botMiddle.addChild(botMiddleGfx);
-  botMiddle.inputEnabled = true;
-  botMiddle.events.onInputDown.add(onDown, this);  
-
-  botRight = game.add.sprite(0, 0);
-  botRight.addChild(botRightGfx);
-  botRight.inputEnabled = true;
-  botRight.events.onInputDown.add(onDown, this);  
+    s = game.add.sprite(0, 0);
+    s.addChild(g);
+    s.data = i;
+    s.inputEnabled = true; 
+  } 
 
   game.input.onDown.add(function(pointer) {
     if (!game.scale.isFullScreen) {
       game.scale.startFullScreen(false);
-    }
-
+    } else {
+        if (pointer.targetObject) {
+       shootEnemy(pointer.targetObject.sprite.data + 1);
+        }
+    }    
   }, this);
-}
-
-function onDown(sprite, pointer) {
-  if(pointer.y <= bttnHeight) {
-      if(pointer.x <= bttnWidth) {
-        shootEnemy(1);
-      } else if (pointer.x > bttnWidth && pointer.x <= bttnWidth * 2) {
-        shootEnemy(2); 
-      } else if (pointer.x >= bttnWidth * 2) {
-        shootEnemy(3);
-      } 
-  } else {
-    if(pointer.x <= bttnWidth) {
-        shootEnemy(4);
-    } else if (pointer.x > bttnWidth && pointer.x <= bttnWidth * 2) {
-        shootEnemy(5);
-    } else if (pointer.x >= bttnWidth * 2) {
-        shootEnemy(6);
-    }
-  }
-}
-
-function update() {
 }
