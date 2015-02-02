ebfca46944881c5345e732e69620b9e73a82b879
Sebastian M. Pedersen <sem.pedersen@stud.uis.no>
2015-02-02 21:04:12 +0100

    Started on the GameSchedulers run function

--- backend/gamescheduler.go
+++ backend/gamescheduler.go
@@ -1,12 +1,40 @@
 package main
 
 type GameScheduler struct {
-	games       []Game
-	launcher    Game
+	games       map[string]Game
+	startGame   chan Game
 	currentGame Game
 }
 
-func (gs *GameScheduler) startGame(id int) {
-	gs.currentGame = gs.games[id]
-	gs.currentGame.start()
+func (gs *GameScheduler) startGame(name string) {
+	if currentGame.Name != "" {
+		return
+	}
+
+	if game, ok := games[name]; ok {
+		startGame <- game
+	}
+}
+
+func (gs *GameScheduler) run() {
+	const launcher = "launcher"
+
+	go gs.startGame(launcher)
+
+	for {
+		select {
+		case g := <-startGame:
+			gs.currentGame = g
+			g.start()
+
+			if gs.currentGame.Name != launcher {
+				go func() {
+					gs.currentGame = Game{}
+					gs.startGame(launcher)
+				}()
+			} else {
+				gs.currentGame = Game{}
+			}
+		}
+	}
 }
