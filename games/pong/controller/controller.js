6bd70f8c671639a1ecb7318673a9670201fd0044
Jonas J. Husebø <jo-huseb@hotmail.com>
2015-02-25 20:32:37 +0100

    Added selectPaddle buttons
    They also send message to game

--- games/pong/controller/controller.js
+++ games/pong/controller/controller.js
@@ -1,10 +1,16 @@
 var game = new Phaser.Game(800, 600, Phaser.AUTO, 'game', {preload: preload, create: create, update: update});
 var sprite;
+var leftBttn;
+var rightBttn;
 var target;
+
+var leftSelected = false;
+var rightSelected = false;
 
 var y = 0;
 
 var movePaddle = function(pos) {};
+var selectPaddle = function(sel){};
 
 function preload() {
   addMessageHandler(function(msg) {
@@ -13,8 +19,12 @@ function preload() {
         sendToGame("move", pos);
       }
     }
+    if (msg.action === "play as") {
+      selectPaddle = function(sel) {
+        sendToGame("play as", sel);
+      }
+    }
   });
-
   game.stage.backgroundColor = '#000000';
 }
 
@@ -35,11 +45,28 @@ function create() {
   sprite.input.allowHorizontalDrag = false;
   sprite.input.boundsRect = new Phaser.Rectangle(32, 32, game.stage.width, game.stage.height - 32 - 128);
 
+  var btnGraphics1 = game.add.graphics(0, 0);
+  btnGraphics1.beginFill(0xFF2245, 1);
+  btnGraphics1.drawRect(0, game.stage.height/2 - 128, game.stage.width/2, 128);
+  leftBttn = game.add.sprite(0, 0);
+  leftBttn.addChild(btnGraphics1);
+  leftBttn.inputEnabled = true;
+  leftBttn.data = 0;
+  var btnGraphics2 = game.add.graphics(0, 0);
+  btnGraphics2.beginFill(0xFF0000, 1);
+  btnGraphics2.drawRect(game.stage.width/2 + 10, game.stage.height/2 - 128, game.stage.width/2 - 10, 128);
+  rightBttn = game.add.sprite(0, 0);
+  rightBttn.addChild(btnGraphics2);
+  rightBttn.inputEnabled = true;
+  rightBttn.data = 1;
+
   game.input.onDown.add(function(pointer) {
     if (!game.scale.isFullScreen) {
       game.scale.startFullScreen(false);
+    } else if (pointer.targetObject) {
+     selectPaddle(pointer.targetObject.sprite.data);
+     console.log(pointer.targetObject.sprite.data);
     }
-
     target = pointer.targetObject;
   }, this);
 }
