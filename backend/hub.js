cf1ec46352d9e34f89db52c42a3607907d4d2af6
Sebastian M. Pedersen <sem.pedersen@stud.uis.no>
2015-04-01 19:19:43 +0200

    Fixed the formating on html/js files

--- backend/hub.js
+++ backend/hub.js
@@ -1,94 +1,104 @@
 var backend
 
 $(function() {
-  disconnect = createButton("Disconnect");
-  disconnect.click(function () {
-    sendToBackend("disconnect");
-  });
+    disconnect = createButton("Disconnect");
+    disconnect.click(function() {
+        sendToBackend("disconnect");
+    });
 
-  quit = createButton("Quit game");
-  quit.click(function () {
-    sendToBackend("start", "Launcher");
-  });
+    quit = createButton("Quit game");
+    quit.click(function() {
+        sendToBackend("start", "Launcher");
+    });
 
-  $("#game").before(disconnect);
-  $("#game").before(quit);
+    $("#game").before(disconnect);
+    $("#game").before(quit);
 });
 
 function createButton(text) {
-  button = $('<button></button>');
-  button.html(text);
-  button.css('width', '50%');
-  button.css('height', '75px');
-  button.css('font-weight', 'bold');
+    button = $('<button></button>');
+    button.html(text);
+    button.css('width', '50%');
+    button.css('height', '75px');
+    button.css('font-weight', 'bold');
 
-  return button;
+    return button;
 }
 
 function addMessageHandler(callback) {
-  backend = new WebSocket('ws://' + window.location.hostname + ':3001/ws');
+    backend = new WebSocket('ws://' + window.location.hostname + ':3001/ws');
 
-  backend.onmessage = function(e) {
-    var msg = JSON.parse(e.data);
+    backend.onmessage = function(e) {
+        var msg = JSON.parse(e.data);
 
-    if (msg.action === "identify") {
-      if (!msg.data) {
-        var id = localStorage.getItem("id-gsusfcavf");
+        if (msg.action === "identify") {
+            if (!msg.data) {
+                var id = localStorage.getItem("id-gsusfcavf");
 
-        if(id == null) {
-          id = Math.random().toString(36).substring(7);
+                if (id == null) {
+                    id = Math.random().toString(36).substring(7);
 
-          localStorage.setItem("id-gsusfcavf", id)
+                    localStorage.setItem("id-gsusfcavf", id)
+                }
+
+                sendToBackend("identify", id);
+            } else if (msg.data === "ok") {
+                callback("identified");
+            } else {
+                var id = sessionStorage.getItem("id-gsusfcavf");
+
+                if (id == null) {
+                    id = Math.random().toString(36).substring(7);
+
+                    sessionStorage.setItem("id-gsusfcavf", id)
+                }
+
+                sendToBackend("identify", id);
+            }
+        } else if (msg.action === "redirect") {
+            var gameName = msg.data.toLowerCase()
+
+            if (document.location.href.indexOf(gameName) < 0) {
+                document.location.href = "/" + gameName;
+            }
+        } else {
+            callback(msg);
         }
-
-        sendToBackend("identify", id);
-      } else if (msg.data === "ok") {
-        callback("identified");
-      } else {
-        var id = sessionStorage.getItem("id-gsusfcavf");
-
-        if(id == null) {
-          id = Math.random().toString(36).substring(7);
-
-          sessionStorage.setItem("id-gsusfcavf", id)
-        }
-
-        sendToBackend("identify", id);
-      }
-    } else if (msg.action === "redirect") {
-      var gameName = msg.data.toLowerCase()
-
-      if (document.location.href.indexOf(gameName) < 0) {
-        document.location.href = "/" + gameName;
-      }
-    } else {
-      callback(msg);
     }
-  }
 }
 
 function sendToGame(action, data) {
-  send({"to": "game", "action": "passthrough", "data": {"action": action, "data": data}});
+    send({
+        "to": "game",
+        "action": "passthrough",
+        "data": {
+            "action": action,
+            "data": data
+        }
+    });
 }
 
 function sendToBackend(action, data) {
-  send({"action": action, "data": data});
+    send({
+        "action": action,
+        "data": data
+    });
 }
 
 function send(json) {
-  if (backend) {
-    backend.send(JSON.stringify(json));
-  }
+    if (backend) {
+        backend.send(JSON.stringify(json));
+    }
 }
 
 function getId() {
-  var id;
+    var id;
 
-  if (sessionStorage.getItem("id-gsusfcavf")) {
-    id = sessionStorage.getItem("id-gsusfcavf");
-  } else {
-    id = localStorage.getItem("id-gsusfcavf");
-  }
+    if (sessionStorage.getItem("id-gsusfcavf")) {
+        id = sessionStorage.getItem("id-gsusfcavf");
+    } else {
+        id = localStorage.getItem("id-gsusfcavf");
+    }
 
-  return id;
+    return id;
 }
--- games/triggerhappy/controller/controller.js
+++ games/triggerhappy/controller/controller.js
@@ -1,48 +1,51 @@
-var game = new Phaser.Game(800, 600, Phaser.AUTO, 'game', {preload: preload, create: create});
+var game = new Phaser.Game(800, 600, Phaser.AUTO, 'game', {
+    preload: preload,
+    create: create
+});
 
 var pos;
 
-var shootEnemy = function(pos) { }
+var shootEnemy = function(pos) {}
 
 
 function preload() {
-  addMessageHandler(function(msg) {
-    if (msg === "identified") {
-      shootEnemy = function(pos) {
-        sendToGame("shoot", pos);
-      }
-    }
-  });
+    addMessageHandler(function(msg) {
+        if (msg === "identified") {
+            shootEnemy = function(pos) {
+                sendToGame("shoot", pos);
+            }
+        }
+    });
 
-  game.stage.backgroundColor = '#2C3E59';
+    game.stage.backgroundColor = '#2C3E59';
 }
 
 function create() {
-  game.scale.fullScreenScaleMode = Phaser.ScaleManager.EXACT_FIT;
-  game.scale.scaleMode = Phaser.ScaleManager.EXACT_FIT;
-  game.scale.refresh();
+    game.scale.fullScreenScaleMode = Phaser.ScaleManager.EXACT_FIT;
+    game.scale.scaleMode = Phaser.ScaleManager.EXACT_FIT;
+    game.scale.refresh();
 
-  var bttnWidth = game.stage.width / 3;
-  var bttnHeight = game.stage.height / 2;
+    var bttnWidth = game.stage.width / 3;
+    var bttnHeight = game.stage.height / 2;
 
-  for (i=0; i < 6; i++) {
-    g = game.add.graphics(0, 0);
-    g.beginFill(0xE74C3C, 1);
-    g.drawRect((i%3) * bttnWidth + 32, (i > 2 ? 1 : 0) * bttnHeight + 32, bttnWidth - 64, bttnHeight - 64);
+    for (i = 0; i < 6; i++) {
+        g = game.add.graphics(0, 0);
+        g.beginFill(0xE74C3C, 1);
+        g.drawRect((i % 3) * bttnWidth + 32, (i > 2 ? 1 : 0) * bttnHeight + 32, bttnWidth - 64, bttnHeight - 64);
 
-    s = game.add.sprite(0, 0);
-    s.addChild(g);
-    s.data = i;
-    s.inputEnabled = true;
-  }
-
-  game.input.onDown.add(function(pointer) {
-    if (!game.scale.isFullScreen) {
-      game.scale.startFullScreen(false);
-    } else {
-      if (pointer.targetObject) {
-        shootEnemy(pointer.targetObject.sprite.data + 1);
-      }
+        s = game.add.sprite(0, 0);
+        s.addChild(g);
+        s.data = i;
+        s.inputEnabled = true;
     }
-  }, this);
+
+    game.input.onDown.add(function(pointer) {
+        if (!game.scale.isFullScreen) {
+            game.scale.startFullScreen(false);
+        } else {
+            if (pointer.targetObject) {
+                shootEnemy(pointer.targetObject.sprite.data + 1);
+            }
+        }
+    }, this);
 }
