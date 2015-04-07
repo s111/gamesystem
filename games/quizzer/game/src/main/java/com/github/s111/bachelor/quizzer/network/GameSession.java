ea9bf136bbaf3628cd66cc27f806fb5cef40f602
Jonas J. Husebø <jo-huseb@hotmail.com>
2015-04-07 23:54:08 +0200

    Added multiplayer functionality and scoring. scoring will be changed in later commits.

--- games/quizzer/game/src/main/java/com/github/s111/bachelor/quizzer/network/GameSession.java
+++ games/quizzer/game/src/main/java/com/github/s111/bachelor/quizzer/network/GameSession.java
@@ -3,9 +3,7 @@ package com.github.s111.bachelor.quizzer.network;
 import com.github.s111.bachelor.quizzer.game.Quizzer;
 import org.glassfish.tyrus.client.ClientManager;
 
-import javax.json.Json;
-import javax.json.JsonObject;
-import javax.json.JsonReader;
+import javax.json.*;
 import javax.websocket.DeploymentException;
 import javax.websocket.EncodeException;
 import javax.websocket.Session;
@@ -13,14 +11,21 @@ import java.io.IOException;
 import java.io.StringReader;
 import java.net.URI;
 import java.net.URISyntaxException;
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
 
 public class GameSession {
     private final Quizzer game;
+
+    private Set<Player> players;
 
     private Session backend;
 
     public GameSession(Quizzer game) throws DeploymentException {
         this.game = game;
+        players = new HashSet<>();
     }
 
     public void connect() throws URISyntaxException, IOException, DeploymentException {
@@ -36,14 +41,58 @@ public class GameSession {
                 .build());
     }
 
-    public void onMessage(Session session, String message) throws IOException {
+    public void onMessage(Session session, String message) throws IOException, EncodeException {
         JsonReader jsonReader = Json.createReader(new StringReader(message));
         JsonObject jsonObj = jsonReader.readObject();
         jsonReader.close();
 
+        if (!(jsonObj.containsKey("action") && jsonObj.containsKey("data"))) {
+            return;
+        }
+
         String action = jsonObj.getString("action");
 
         switch (action) {
+            case "identify": {
+                String data = jsonObj.getString("data");
+
+                if (data.equals("ok")) {
+                    backend.getBasicRemote().sendObject(Json.createObjectBuilder()
+                            .add("action", "get clients")
+                            .build());
+                }
+            }
+            case "get clients": {
+                if (jsonObj.isNull("data")) {
+                    break;
+                }
+
+                JsonArray clients = jsonObj.getJsonArray("data");
+
+                for (JsonValue client : clients) {
+                    players.add(new Player(client.toString()));
+                }
+
+                break;
+            }
+            case "added client": {
+                String id = jsonObj.getString("data");
+
+                players.add(new Player(id));
+
+                break;
+            }
+            case "dropped client": {
+                String id = jsonObj.getString("data");
+
+                for (Player player : players) {
+                    if (player.getId().equals(id)) {
+                        players.remove(player);
+                        break;
+                    }
+                }
+                break;
+            }
             case "answer": {
                 String data = jsonObj.getJsonNumber("data").toString();
 
@@ -54,8 +103,60 @@ public class GameSession {
                     selection = -1;
                 }
 
+                if (!jsonObj.containsKey("from")) {
+                    return;
+                }
+
+                String from = jsonObj.getString("from");
+
+                for (Player player : players) {
+                    if (player.getId().equals(from)) {
+                        if (game.checkIfCorrectAnswer(selection)) {
+                            player.increaseScore();
+                        }
+                    }
+                }
                 game.checkIfCorrectAnswer(selection);
             }
+        }
+    }
+
+    public List<Integer> getScores() {
+        List<Integer> scores = new ArrayList<Integer>();
+        for (Player player : players) {
+            scores.add(player.getScore());
+        }
+        return scores;
+    }
+
+    private class Player {
+        private String id;
+        private int score = 0;
+
+        private Player(String id) {
+            this.id = id;
+        }
+
+        public String getId() {
+            return id;
+        }
+
+        public void increaseScore() {
+            score++;
+        }
+
+        public int getScore() {
+            return score;
+        }
+
+        @Override
+        public boolean equals(Object o) {
+            return ((Player) o).getId().equals(getId());
+        }
+
+        @Override
+        public int hashCode() {
+            return getId().hashCode();
         }
     }
 }
