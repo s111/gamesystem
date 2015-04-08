7b4a0b4e8bf8f82d009623733a9a5cf1eefb0709
Fredrik Kallevik <fredrik.kallevik@gmail.com>
2015-04-08 21:39:55 +0200

    Game info changes according to the games selected
    With the exception of the screenshot (TODO)
    js-beautify was run.

--- games/launcher/game/launcher.js
+++ games/launcher/game/launcher.js
@@ -12,6 +12,7 @@ $(function() {
                 $.each(msg.data, function(index, game) {
                     $("#games").append("<li id=\"lst" + game + "\">" + game + "</li>");
                 });
+
                 break;
             case "select":
                 $("#games li").each(function(index) {
@@ -20,10 +21,28 @@ $(function() {
 
                 $("#lst" + msg.data).addClass("selected");
 
+                $("#title").html(msg.data);
+
+                sendToBackend("get players", msg.data);
+                sendToBackend("get description", msg.data);
+
                 break;
             case "start":
                 sendToBackend("start", msg.data);
                 gui.App.quit();
+
+                break;
+            case "get description":
+                $("#desc-text").html(msg.data);
+
+                break;
+            case "get players":
+                if (msg.data === 0) {
+                    $("#players").html(" Unlimited");
+                } else {
+                    $("#players").html(" " + msg.data);
+                }
+
                 break;
         }
     });
@@ -31,8 +50,10 @@ $(function() {
 
 function addMessageHandler(callback) {
     backend = new WebSocket('ws://localhost:3001/ws');
+
     backend.onmessage = function(e) {
         var msg = JSON.parse(e.data);
+
         if (msg.action === "identify") {
             if (!msg.data) {
                 sendToBackend("identify", "game");
